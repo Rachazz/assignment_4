0f04056a88f473f6955ef256dc29312aebf2ca1a
Rachazz <40149285+Rachazz@users.noreply.github.com>
2019-06-19 17:48:31 -0500

    a

--- application.py
+++ application.py
@@ -4,21 +4,113 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 import random
-
+import numpy as np
+import mysql.connector
 
 app = Flask(__name__)
+
+config = {
+  'host':'demoquakes.mysql.database.azure.com',
+  'user':'quakes@demoquakes',
+  'password':'Earth_quake',
+  'database':'equakes'
+}
+
 @app.route('/')
-def plot():
-    plt.plot([1,2,3],[5,6,7])
+def piechart():
+    conn = mysql.connector.connect(**config)
+    cursor = conn.cursor()
+    print("Connection....")
+    ##query="select count(*) from earthquake where mag>8"
+    ##cursor.execute(query)
+
+    ##result_set = cursor.fetchall()
+    ##age =[]
+    #for row in result_set:
+       #age.append(row[0])
+
+    age1=[1,2,3]
+    p_labels=[1,2,3]
+
+
+
+
+    plt.pie(age1,labels=p_labels)
+
     plt.show()
 
-    return "Success"
+    return render_template('display.html')
 
 
 if __name__ == '__main__':
   app.run()
+'''
+def plot():
+    conn = mysql.connector.connect(**config)
+    cursor = conn.cursor()
+    print("Connection....")
+   
+    row=[]
+    #query='select count(*) from earthquake where "depthError" between 0 and 1'
+    #query="select time,latitude,longitude,depthError from earthquake where depthError between 0 and 3"
+    #query="select count(*) from earthquake where depthError between 0 and 0.5"
+    query="select latitude,longitude from earthquake where depthError between 0 and 0.2"
+    cursor.execute(query)
+    row = cursor.fetchall()
+
+    lat=[]
+    long=[]
+    for i in row:
+        lat.append(row[0])
+        long.append(row[1])
+
+    print(type(list(lat)))
+    print("----------------------")
+
+
+    plt.plot(lat,long)
+    plt.xlabel('x-axis')
+    plt.ylabel('y-axis')
+    plt.show()
 
 
 
+
+    return render_template('display.html')
+
+
+
+
+
+
+
+def barchart():
+
+    conn = mysql.connector.connect(**config)
+    cursor = conn.cursor()
+    print("Connection....")
+    query="select latitude,longitude from earthquake where depthError between 0 and 0.2"
+
+    cursor.execute(query)
+    result_set = cursor.fetchall()
+    lat =[]
+    lon=[]
+    for row in result_set:
+        lat.append(row[0])
+        lon.append(row[1])
+
+
+    objects = ('1', '2')
+    y_pos = np.arange(len(objects))
+    performance = [lat[0]]
+
+    plt.bar(y_pos, performance, align='center', alpha=0.5)
+    plt.xticks(y_pos, objects)
+    plt.ylabel('Count')
+    plt.title('numbers of male survivors')
+    plt.show()
+
+    return render_template('display.html')
+'''
 
 
