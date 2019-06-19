d6c36ca54065db788fc52cc4ec69acaf2eeabd4c
Rachazz <40149285+Rachazz@users.noreply.github.com>
2019-06-19 14:27:40 -0500

    7

--- application.py
+++ application.py
@@ -1,16 +1,24 @@
-from flask import Flask,render_template,request
-
+from flask import Flask,render_template,make_response
+import io
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import random
+
 
 app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    plt.plot([1,2,3],[5,6,4])
+@app.route('/')
+def plot():
+    plt.plot([1,2,3],[5,6,7])
     plt.show()
 
-    return "Success..."
+    return "Success"
+
 
 if __name__ == '__main__':
   app.run()
+
+
+
+
 
