07d632954a7242b9d2a56528cd7865ad1f4db43d
Rachazz <40149285+Rachazz@users.noreply.github.com>
2019-06-19 12:57:09 -0500

    6

--- application.py
+++ application.py
@@ -1,6 +1,23 @@
-from flask import Flask
+from flask import Flask,render_template,request
+import mysql.connector
+import matplotlib.pyplot as plt
+
 app = Flask(__name__)
+
+config = {
+  'host':'demoquakes.mysql.database.azure.com',
+  'user':'quakes@demoquakes',
+  'password':'Earth_quake',
+  'database':'equakes'
+}
 
 @app.route("/")
 def hello():
-    return "Hello World finally. done!"
+    plt.plot([1,2,3],[5,6,4])
+    plt.show()
+
+    return "Success"
+
+if __name__ == '__main__':
+  app.run()
+
